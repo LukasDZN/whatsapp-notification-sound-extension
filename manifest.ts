389423cde3a6e43260e7477ce8c345487e1d9d74
LukasDZN <Dzenk.Lukas@gmail.com>
2023-03-16 03:45:47 +0200

    minor

--- manifest.ts
+++ manifest.ts
@@ -5,7 +5,8 @@ import packageJson from './package.json'
  */
 const manifest: chrome.runtime.ManifestV3 = {
   manifest_version: 3,
-  name: packageJson.name,
+  // name: packageJson.name,
+  name: 'WhatSound - Change WhatsApp Web notification sound',
   version: packageJson.version,
   description: packageJson.description,
   // options_page: "src/pages/options/index.html",
